e10e3f4227353da19f22fbf4560d8eec0dc6de5c
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2018-07-09 09:40:10 +0100

    Add linethrough, remove and all done

--- src/App.js
+++ src/App.js
@@ -37,12 +37,33 @@ class App extends Component {
 
 	toggleTodoDone(event, index) {
 		console.log(event.target.checked);
-		const todos = [...this.state.todos];
-		const todo = {...todos[index]};
-		todo.done = event.target.checked;
+		const todos = [...this.state.todos]; // copy the array
+		todos[index] = {...todos[index]}; // copy the todo
+		todos[index].done = event.target.checked; // update done property on copied
 		this.setState({
 			todos
 		}); 
+	}
+
+	removeTodo(index) {
+		const todos = [...this.state.todos]; // copy the array
+		todos.splice(index, 1);
+		this.setState({
+			todos
+		});
+	}
+
+	allDone() {
+		const todos = this.state.todos.map(todo => {
+			return {
+				title: todo.title,
+				done: true
+			}
+		});
+
+		this.setState({
+			todos
+		});
 	}
 
 	render() {
@@ -54,12 +75,15 @@ class App extends Component {
 					<input onChange={(event) => this.newTodoChanged(event)} id="newTodo" name="newTodo" value={this.state.newTodo} />
 					<button type="submit">Add Todo</button>
 				</form>
+				<button onClick={() => this.allDone()}>All Done</button>
 				<ul>
 					{this.state.todos.map((todo, index) => {
 						return (
 							<li key={todo.title}>
-								<input onChange={(event) => this.toggleTodoDone(event, index)} type="checkbox" /> 
-								{todo.title}
+								<input onChange={(event) => this.toggleTodoDone(event, index)} type="checkbox" checked={todo.done}/> 
+								{/*} <span style={{ textDecoration: todo.done ? 'line-through' : 'inherit' }}>{todo.title}</span> */}
+								<span className={todo.done ? 'done' : ''}>{todo.title}</span>
+								<button onClick={()=> this.removeTodo(index)}>Remove</button>
 							</li>
 						)
 					})}
